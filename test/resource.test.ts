844f989fcdb51d35729b909da2af664ce748a603
Jakob Durstberger <jakob@durstberger.me>
2023-11-23 19:39:20 +0100

    Fixes resource test name

--- test/resource.test.ts
+++ test/resource.test.ts
@@ -2,7 +2,7 @@ import { Resource } from '../src'
 
 describe('HAL Resource', () => {
   describe('to JSON', () => {
-    test('creates empty resource', () => {
+    test('creates empty JSON for new resource', () => {
       const resource = Resource.create()
 
       const json = resource.toJson()
